437320b6c225242708d7e836adb276cb501ed5e1
sujileelea <sujileelea@gmail.com>
2024-08-27 19:33:47 +0900

    feature: logout and user delete with button -> alert process

--- Blindar/View/Settings/SettingPage.swift
+++ Blindar/View/Settings/SettingPage.swift
@@ -6,14 +6,19 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import SwiftData
 
 struct SettingPage: View {
+    @Environment(\.modelContext) private var modelContext
     @EnvironmentObject var uiManager: UIManager
     @EnvironmentObject var userVM: UserViewModel
     @State var isOnedayModeOn: Bool = false
     @State var isDailyNotificationOn: Bool = false
     @State var settingFeature: SettingFeature = .none
     @Binding var mainPageMode: MainPageMode
+    @State var showLogoutAlert: Bool = false
+    @State var showUserDeleteAlert: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -33,18 +38,6 @@ struct SettingPage: View {
                     Text(userVM.user?.name ?? "이름 정보 없음")
                         .font(.title2)
                     Spacer()
-                    //로그아웃
-                    //                    Button(action: {
-                    //                        
-                    //                    }, label: {
-                    //                        RoundedRectangle(cornerRadius: 8)
-                    //                            .foregroundColor(.hex00497B)
-                    //                            .frame(width: uiManager.screenHeight * 0.11, height: uiManager.screenWidth * 0.1)
-                    //                            .overlay {
-                    //                                Text("로그아웃")
-                    //                                    .foregroundStyle(Color.white)
-                    //                            }
-                    //                    })
                 }
                 VStack(alignment: .leading, spacing: 30) {
                     //하루씩 보기 모드
@@ -69,12 +62,80 @@ struct SettingPage: View {
                         .foregroundColor(.white)
                     })
                 }
+                HStack(spacing: 70) {
+                    //로그아웃
+                    Button(action: {
+                        showLogoutAlert = true
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(.hex00497B)
+                            .frame(width: uiManager.screenHeight * 0.11, height: uiManager.screenWidth * 0.1)
+                            .overlay {
+                                Text("로그아웃")
+                                    .foregroundStyle(Color.white)
+                            }
+                    })
+                    //회원 탈퇴
+                    Button(action: {
+                        showUserDeleteAlert = true
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(.hex00497B)
+                            .frame(width: uiManager.screenHeight * 0.11, height: uiManager.screenWidth * 0.1)
+                            .overlay {
+                                Text("회원 탈퇴")
+                                    .foregroundStyle(Color.white)
+                            }
+                    })
+                }
                 Spacer()
             }
             .padding(.horizontal, 12)
         }
+        .alert(isPresented: $showLogoutAlert) {
+            Alert(title: Text("로그아웃"), primaryButton: .destructive(Text("확인"), action: {
+                logout()
+            }), secondaryButton: .cancel(Text("취소")))
+        }
+        .alert(isPresented: $showUserDeleteAlert) {
+            Alert(title: Text("회원 탈퇴"), message: Text("회원의 모든 정보가 삭제됩니다"), primaryButton: .destructive(Text("확인"), action: {
+                deleteUser()
+            }), secondaryButton: .cancel(Text("취소")))
+        }
         .onAppear {
             isOnedayModeOn = mainPageMode == .oneday
+        }
+    }
+    
+    private func logout() {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    private func deleteUser() {
+        //firebase 계정 삭제
+        let user = Auth.auth().currentUser
+
+        user?.delete { error in
+          if let error = error {
+            print("An error happened")
+          } else {
+            print("Account deleted")
+          }
+        }
+        //로컬 데이터 삭제
+        do {
+            try modelContext.delete(model: MealLocalData.self)
+            try modelContext.delete(model: MemoLocalData.self)
+            try modelContext.delete(model: ScheduleLocalData.self)
+            try modelContext.delete(model: SchoolLocalData.self)
+            try modelContext.delete(model: UserLocalData.self)
+        } catch {
+            print("Failed to clear all Model Context data.")
         }
     }
 }
